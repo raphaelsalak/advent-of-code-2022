db96b70da7ece0c58908b88b99048ad3bde12463
raphael <salak.raphael@heb.com>
2023-06-22 11:12:31 -0500

    fix index is out of range bug

--- day6.py
+++ day6.py
@@ -2,20 +2,27 @@ with open('input/input6.txt') as file:
     contents = file.read().strip()
 
 #parse the whole thing character by character
-inc = 0
+inc = 1
 counter = 0
-string = contents[0] 
-for i in range(1, len(contents)):
+string = contents[0]
+print(contents)
+for i, char in enumerate(contents):
+    start = i+1
+    end = i+5
+    for j in range(start, end):
     #store the characters 4 at a time
-    if contents[i] not in string:
-        string += contents[i]
-        inc += 1
-        if inc == 3:
-            counter += 1
+    #stores the first character fine
+        #if j doesn't equal i
+        #add that to the string
+        if contents[j] != char:
+            string += contents[j]
+            inc += 1
+            if inc == 4:
+                counter = end
+                break
+        else:
             inc = 0
-print('Occurences: ', counter)
-
-
+print('index', counter + 1)
     # , as you store them 
     # check if the incoming character is in the string. 
     # if it is, restart the 4 increment. 
