6c5555317f4fa3bf9bbe0c63c7e09dbaac5c50d9
raphael <salak.raphael@heb.com>
2023-07-01 19:23:55 -0500

    part 1

--- day8.py
+++ day8.py
@@ -1,49 +1,19 @@
-#contents is basically a 2d array already
+
 with open('input/day8.in') as file:
-    contents = [i.strip() for i in file.readlines()]
+     data = [row.strip() for row in file.readlines()]
+ROWS = len(data)
+COLUMNS = len(data[0])
+#There's overlap in rows and columns so columns - 2 to get rid of the ones ROWS already has
+edges = (ROWS * 2) + ((COLUMNS-2)*2)
+total = edges
+for row in range(1, ROWS -1):
+    for col in range(1, COLUMNS-1):
+        tree = data[row][col]
+        right = [data[row][col-i] for i in range(1, col+1)]
+        left = [data[row][col+i] for i in range(1, COLUMNS-col)]
+        up = [data[row-1][col] for i in range(1, row+1)]
+        down = [data[row+i][col-i] for i in range(1, ROWS-row)]
 
-rows, cols = (len(contents), len(contents[0]))
-count = 0
-t = [[0] * cols for _ in range(rows)]
-#need to load it in a 2d array b/c we only have access to one line at a time and we need to compare multiple rows and cols
-for i in range(rows):
-    for j in range(cols):
-        t[i][j] = int(contents[i][j])
-
-#outside edges
-count += (2 * rows) + (2 * cols)
-leftvisible = True
-rightvisible = True
-upvisible = True
-downvisible = True
-
-#inside edges
-for i in range(1, rows-1):
-    for j in range(1, cols-1):
-        up = i - 1
-        down = i + 1 
-        left = j - 1
-        right = j + 1
-        while left != 0:
-            #if the left is greater at any point just break, the curr tree can't be seen
-            #separate the case that it ends and it encounters a tree that's blocking the curr one
-            if t[i][j] <= t[i][left]:
-                leftvisible = False
-            left -= 1
-        while right != cols-1:
-            if t[i][j] <= t[i][right]:
-                rightvisible = False
-            right += 1
-        while up != 0:
-            if t[i][j] <= t[up][j]:
-                upvisible = False
-            up -= 1 
-        while down != rows-1:
-            if t[i][j] <= t[down][j]:
-                downvisible = False
-            down += 1
-        if leftvisible or rightvisible or downvisible or upvisible:
-            count += 1
-print('count', count)
-        
-#When it breaks does it go to the top for loop or does it go to the loop under it?
+        if max(left) <tree or max(right)<tree or max(up)<tree or max(down)<tree:
+               total += 1
+print('count', total)
