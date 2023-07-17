4b20368bb2e9e9c205216e91cfdc08241af5978c
raphael <salak.raphael@heb.com>
2023-07-17 08:42:47 -0500

    learned python debugger

--- day13.py
+++ day13.py
@@ -2,6 +2,7 @@ def appendLevel(alist, level, something):
     if level == 1:
         alist.append(something)
     elif level == 2:
+        print(alist)
         alist[0].append(something)
     elif level == 3:
         alist[0][0].append(something)
@@ -27,20 +28,20 @@ with open('input/day13.in') as file:
 #need to somehow parse each line string and convert it to a list? or parse and solve using the string '[]' ?
 #how to convert string input to list
 a = []
-level = 1
-ignore = [']', ',']
+level = 0
+ignore = [']', ',', '[']
 for i in contents:
     first, second = i.split('\n')
-    maxlength = length(first)
-    print(maxlength)
+    #maxlength = length(first)
     for c in first:
         if c == '[':
             appendLevel(a, level, [])
             level += 1
-        else:
-            if c not in ignore:
+        elif c not in ignore:
+                level -= 1
+                #read in comma to get the whole number e.g. 1 to 10.
                 num = int(c)
-                a = appendLevel(a, level, num)
+                appendLevel(a, level, num)
                 #append num to the current level
 print(a)
 
